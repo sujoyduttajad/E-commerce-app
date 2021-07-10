e1affd0fba07788b4d85dd094424b353bca1ba70
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-10 18:17:13 +0530

    added routes to the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import Result from "./pages/Result"
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/result' component={Result} />
+        <Route path='/:productId' component={Product} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
