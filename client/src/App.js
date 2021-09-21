9ca49176b2c078a4a8db75060bc35e98c2b01f01
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-21 16:21:40 +0530

    Initialized the source code and setup the router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from "./pages/Home"
-import Product from "./pages/Product"
-import Result from "./pages/Result"
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/result' component={Result} />
-        <Route path='/:productId' component={Product} />
-      </Switch>
-    </Router>
-  );
+  return <div>App</div>;
 }
 
 export default App;
