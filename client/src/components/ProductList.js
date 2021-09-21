00124a2a3930e2e96d3190e054a1deb05000d0cc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-21 16:31:29 +0530

    Update ProductList.js

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from 'axios'
 
 export default function ProductList() {
+
+  useEffect(() => {
+
+  }, []);
+
   return <div>product list</div>;
 }
 
