e266559e7913f4d29545169afdb644d42cbe5a79
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-22 16:38:55 +0530

    replaced divs into fragments and removed the Link tags for now

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, Router } from 'react-router-dom';
 
 export default function ProductList() {
 
@@ -13,7 +13,7 @@ export default function ProductList() {
   }, []);
 
   return (
-    <div>
+    <>
       {
         products.map(product => (
             <ProductItem
@@ -22,21 +22,26 @@ export default function ProductList() {
             />
         ))
       }
-    </div>
+    </>
   )
 }
 
 function ProductItem({ product }) {
+
+  // {/* Just temporarily will be shifted to App.js root level comp */}
+    // </Router>
+
   return (
+      
     <div className="p-4 md:w-1/3">
       <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-        <Link to={`/${product.id}`}>
+        {/* <Link to={`/${product.id}`}> */}
           <img
             className="lg:h-96 md:h-36 w-full object-cover object-center"
             src={product.image}
             alt={product.name}
           />
-        </Link>
+        {/* </Link> */}
         <div className="p-6">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
             {product.category}
@@ -48,20 +53,22 @@ function ProductItem({ product }) {
             {product.description}
           </p>
           <div className="flex items-center flex-wrap ">
-            <Link to={`/${product.id}`} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
-              See More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </Link>
+            {/* <Link to={`/${product.id}`} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0"> */}
+              <span className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                See More
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </span>
+            {/* </Link> */}
             <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pr-3 py-1 border-gray-800 font-bold">
               {product.price}
             </span>
@@ -69,5 +76,7 @@ function ProductItem({ product }) {
         </div>
       </div>
     </div>
+
+    
   );
 }
