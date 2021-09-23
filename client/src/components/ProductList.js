e41981bebe99d4ce3de006105e9c4d8c6c54b3fa
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-23 10:43:26 +0530

    Added Link to individual Product page

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -35,13 +35,13 @@ function ProductItem({ product }) {
       
     <div className="p-4 md:w-1/3">
       <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-        {/* <Link to={`/${product.id}`}> */}
+        <Link to={`/${product.id}`}>
           <img
             className="lg:h-96 md:h-36 w-full object-cover object-center"
             src={product.image}
             alt={product.name}
           />
-        {/* </Link> */}
+        </Link>
         <div className="p-6">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
             {product.category}
@@ -53,8 +53,8 @@ function ProductItem({ product }) {
             {product.description}
           </p>
           <div className="flex items-center flex-wrap ">
-            {/* <Link to={`/${product.id}`} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0"> */}
-              <span className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+            <Link to={`/${product.id}`} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+              {/* <span className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0"> */}
                 See More
                 <svg
                   fill="none"
@@ -67,8 +67,8 @@ function ProductItem({ product }) {
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
-              </span>
-            {/* </Link> */}
+              {/* </span> */}
+            </Link>
             <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pr-3 py-1 border-gray-800 font-bold">
               {product.price}
             </span>
