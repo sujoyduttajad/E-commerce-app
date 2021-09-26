bea61b06eed99b6bf4177898e93f03a806eba581
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-26 15:21:27 +0530

    Added productID specific a get request route

--- server/routes.js
+++ server/routes.js
@@ -5,10 +5,18 @@ module.exports = function getRoutes() {
   const router = express.Router();
 
   router.get('/products', getProducts);
+  router.get('/products/:productId', getSingleProduct);
 
   return router;
 };
 
 function getProducts(req, res) {
   res.status(200).json({ products });
+}
+
+function getSingleProduct(req, res) {
+  const { productId } = req.params;
+  const product = products.find((product) => product.id === productId);
+
+  res.status(200).json({ product });
 }
