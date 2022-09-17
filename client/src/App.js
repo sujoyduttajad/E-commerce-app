140a8212cf21ff1f41aa526ce283f2c254160a54
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-18 03:34:40 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { CartProvider } from "use-shopping-cart";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE);
@@ -19,6 +20,7 @@ function App() {
         currency="USD"
       >
         <QueryClientProvider client={queryClient}>
+          <Toaster position="bottom-center" />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/result" component={Result} />
