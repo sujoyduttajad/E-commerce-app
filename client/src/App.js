1ec73ac56841cefeb8396706e6b82c7fdb3ed247
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-17 19:37:02 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import Home from './pages/Home';
-import Result from './pages/Result';
-import Product from './pages/Product';
-import { QueryClientProvider, QueryClient } from "react-query";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import Product from "./pages/Product";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
 
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/result" component={Result} />
           <Route path="/:productId" component={Product} />
         </Switch>
-      </BrowserRouter>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
