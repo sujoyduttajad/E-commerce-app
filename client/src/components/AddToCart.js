6730b33a53a56653b56f0346602f83950a5f8342
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-18 03:34:38 +0530

    Update AddToCart.js

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -1,13 +1,19 @@
 import React from "react";
+import toast from "react-hot-toast";
 import { useShoppingCart } from "use-shopping-cart";
 
 export default function AddToCart({ product }) {
   const { addItem } = useShoppingCart();
 
+  const handleAddItem = () => {
+    addItem(product);
+    toast.success(`${product.name} has been added to your cart!`)
+  }
+
   return (
     <button 
       className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
-      onClick={() => addItem(product)}
+      onClick={handleAddItem}
     >
       Add To Cart
     </button>
