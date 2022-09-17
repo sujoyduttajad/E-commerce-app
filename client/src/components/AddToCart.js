0e43a61b5e0c70e066861357492fe6c9dfe76376
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-18 03:26:09 +0530

    Update AddToCart.js

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -2,11 +2,13 @@ import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
 
 export default function AddToCart({ product }) {
-
   const { addItem } = useShoppingCart();
-  
+
   return (
-    <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+    <button 
+      className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
+      onClick={() => addItem(product)}
+    >
       Add To Cart
     </button>
   );
