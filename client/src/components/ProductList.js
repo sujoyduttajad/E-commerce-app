d66be4b866487047db42db279c09fc711d12fc7a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-17 23:13:26 +0530

    Update ProductList.js

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "react-query";
 import LoadingSpinner from "./LoadingSpinner";
 import formatProductPrice from "../utils/formatProductPrice";
 
