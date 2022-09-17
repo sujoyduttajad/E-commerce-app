203e37318785e0e1d596a4d5f49063837afc3c35
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-17 23:30:45 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,12 +7,12 @@ export default function Home() {
       <div className="container px-5 py-24 mx-auto">
         <div className="text-center mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">
-            E-Commerce App with Complete Shopping Cart
+            EssenCart
           </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-400 text-opacity-80">
+          {/* <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-400 text-opacity-80">
             A full-stack ecommerce app made with a Create-React-App client and
             Node API, using Stripe to process payments.
-          </p>
+          </p> */}
           <div className="flex mt-6 justify-center">
             <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
           </div>
