dc8d742d707cfe0baf68635da15e9f4040e98269
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-17 13:37:20 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const products = require('./products.json')
+const products = require("./products.json");
 
 module.exports = function getRoutes() {
   const router = express.Router();
 
-  router.get('/products', getProducts);
-  router.get('/products/:productId', getSingleProduct);
+  router.get("/products", getProducts);
+  router.get("/products/:productId", getSingleProduct);
 
   return router;
 };
@@ -18,5 +18,12 @@ function getSingleProduct(req, res) {
   const { productId } = req.params;
   const product = products.find((product) => product.id === productId);
 
-  res.status(200).json({ product });
+  try {
+    if (!product) {
+      throw Error(`No product found for id: ${productId} `);
+    }
+    res.status(200).json({ product });
+  } catch (err) {
+    return res.status(404).json({ statusCode: 404,  message: error.message });
+  }
 }
