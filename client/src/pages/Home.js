151884073643c921a389f1d4a75f760c4812e150
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-23 16:45:58 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,14 +5,11 @@ export default function Home() {
   return (
     <section className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <div className="text-center mb-20">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font text-white mb-4">
-            EssenCart
-          </h1>
+        {/* <div className="text-center mb-20">
           <div className="flex mt-6 justify-center">
             <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
           </div>
-        </div>
+        </div> */}
         <div className="flex flex-wrap -m-4">
           <ProductList />
         </div>
