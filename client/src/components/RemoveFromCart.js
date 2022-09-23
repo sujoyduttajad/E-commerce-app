63cf8a875c8746389d08203c0d837b0dfa4d495d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-23 15:35:37 +0530

    Update RemoveFromCart.js

--- client/src/components/RemoveFromCart.js
+++ client/src/components/RemoveFromCart.js
@@ -3,19 +3,22 @@ import toast from "react-hot-toast";
 import { useShoppingCart } from "use-shopping-cart";
 
 export default function RemoveFromCart({ product }) {
-
   const { removeItem, cartCount } = useShoppingCart();
 
   const handleRemoveItem = () => {
     removeItem(product.sku);
-    toast.success(`${product.name} is removed from your cart!`)
-  }
+    toast.success(`${product.name} is removed from your cart!`);
+  };
 
   return (
-    <button 
+    <button
       className="flex ml-2 text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
       onClick={handleRemoveItem}
       disabled={!cartCount}
+      style={{
+        cursor: `${cartCount > 0 ? "pointer" : "not-allowed"}`,
+        opacity: `${cartCount > 0 ? 1 : 0.5}`,
+      }}
     >
       Remove
     </button>
