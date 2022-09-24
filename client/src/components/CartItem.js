5c223402d4b9047d05d202414d95e31efea1655d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-24 19:56:33 +0530

    Update CartItem.js

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,18 +1,19 @@
 import React from "react";
+import formatProductPrice from "utils/formatProductPrice";
 
-export default function CartItem() {
+export default function CartItem({ cartItem }) {
   return (
     <div className="flex w-full">
       <div className="flex items-center px-4 py-3 hover:bg-gray-100 -mx-4 w-full justify-between">
         <div className="flex">
           <img
             className="h-16 w-16 rounded-full object-cover mx-1"
-            src="https://dummyimage.com/200x200"
-            alt="avatar"
+            src={cartItem.image}
+            alt={cartItem.name}
           />
           <p className="text-gray-600 text-lg mx-2">
-            <span className="font-bold">Name</span> <br />
-            Price x Quantity
+            <span className="font-bold">{cartItem.name}</span> <br />
+            {formatProductPrice(cartItem)} x {cartItem.quantity}
           </p>
         </div>
         <div>
