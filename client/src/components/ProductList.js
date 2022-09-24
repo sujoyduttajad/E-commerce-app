0e5135574424b9b4c77027352e77ae6ee643a274
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-24 16:08:12 +0530

    Update ProductList.js

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
