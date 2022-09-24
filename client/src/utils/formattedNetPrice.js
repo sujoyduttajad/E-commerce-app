2ad9af3d6e604eca49f0fdcb7fc52ceafebe8a3a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-24 19:33:20 +0530

    Update formattedNetPrice.js

--- client/src/utils/formattedNetPrice.js
+++ client/src/utils/formattedNetPrice.js
@@ -1,11 +1,9 @@
 import { formatCurrencyString } from "use-shopping-cart";
 
 export default function formattedNetPrice(totalPrice) {
-    
-    return formatCurrencyString({
-        value: totalPrice,
-        currency: 'USD',
-        language: navigator.language,
-    });
-
+  return formatCurrencyString({
+    value: totalPrice,
+    currency: "USD",
+    language: navigator.language,
+  });
 }
