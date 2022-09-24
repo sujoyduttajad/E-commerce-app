d1327f52facc700253aa00c07c2a0b618b2b5430
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-24 17:16:54 +0530

    Fixed the formatted total price value

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import formattedNetPrice from "utils/formattedNetPrice";
 import { CartIcon } from "./Icons";
 
 export default function CartSummary() {
-  const { cartCount, totalPrice } = useShoppingCart();
-  console.log(totalPrice)
+
+  const price = localStorage.getItem("cart-values");
+  const netPrice = JSON.parse(price).totalPrice;
+  const cartCountLS = JSON.parse(price).cartCount;
+  
   return (
     <>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <span className="mr-5 hover:text-white flex items-center">
           <CartIcon />
           <span className="ml-3">
-            ({cartCount})
+            {formattedNetPrice(netPrice)} ({cartCountLS})
           </span>
         </span>
       </nav>
