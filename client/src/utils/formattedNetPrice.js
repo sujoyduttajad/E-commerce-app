8bc207798b016ceabab4e058454e9a92e2947d99
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-26 13:12:23 +0530

    Delete formattedNetPrice.js

--- client/src/utils/formattedNetPrice.js
+++
@@ -1,9 +0,0 @@
-import { formatCurrencyString } from "use-shopping-cart";
-
-export default function formattedNetPrice(totalPrice) {
-  return formatCurrencyString({
-    value: totalPrice,
-    currency: "USD",
-    language: navigator.language,
-  });
-}
