6338f086843ac551a9c90b586837ace10f7d5581
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-27 03:16:49 +0530

    Update RemoveFromCart.js

--- client/src/components/RemoveFromCart.js
+++ client/src/components/RemoveFromCart.js
@@ -12,7 +12,8 @@ export default function RemoveFromCart({ product }) {
 
   return (
     <button
-      className="flex ml-2 text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
+      className="flex ml-2 text-white bg-yellow-700 border-0 py-2 px-6 
+      focus:outline-none hover:bg-yellow-600 rounded"
       onClick={handleRemoveItem}
       disabled={!cartCount}
       // style={{
