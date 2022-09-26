3f14e4538c92509cde269ba990499796e06ba35c
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-27 03:16:46 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,11 +5,6 @@ export default function Home() {
   return (
     <section className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-24 mx-auto">
-        {/* <div className="text-center mb-20">
-          <div className="flex mt-6 justify-center">
-            <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
-          </div>
-        </div> */}
         <div className="flex flex-wrap -m-4">
           <ProductList />
         </div>
