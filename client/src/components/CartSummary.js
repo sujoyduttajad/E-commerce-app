4b643b67a55c87e36f3a554c0cf3ce22308434ec
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-27 02:47:49 +0530

    Update CartSummary.js

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -13,9 +13,9 @@ export default function CartSummary() {
     <React.Fragment>
       <button
         onClick={toggleModal}
-        className="focus:outline-none bg-transparent hover:bg-gray-700 rounded mr-3 md:ml-auto p-1 pl-3 flex flex-wrap items-center text-base justify-center cursor-pointer"
+        className="focus:outline-none bg-transparent hover:bg-gray-700 hover:text-white rounded mr-3 md:ml-auto p-1 pl-3 flex flex-wrap items-center text-base justify-center cursor-pointer"
       >
-        <span className="mr-5 hover:text-white flex items-center">
+        <span className="mr-5 flex items-center">
           <CartIcon />
           <span className="ml-3">
             {formattedTotalPrice} ({cartCount})
