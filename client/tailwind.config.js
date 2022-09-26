722fcf5e244b1ec4e1f6fb8af360fafa32b33bd8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-27 03:06:02 +0530

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#111827",
+        secondary: "#ff9a62",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        // 'metal': '#565584',
+        // 'tahiti': '#3ab7bf',
+        // 'silver': '#ecebff',
+        // 'bubble-gum': '#ff77e9',
+        // 'bermuda': '#78dcca',
+      },
+    },
+  },
+};
