9bdd0ddc5d8068d8f76e607e2fa902e701bb5e7a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-27 12:52:27 +0530

    Update useCheckout.js

--- client/src/utils/useCheckout.js
+++ client/src/utils/useCheckout.js
@@ -1,1 +1,16 @@
-export default function useCheckout() {}
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useShoppingCart } from "use-shopping-cart";
+
+export default function useCheckout() {
+    const { redirectToCheckout, cartDetails } = useShoppingCart();
+    
+    async function handleCheckout() {
+        const session = await axios.post('/api/checkout-sessions', cartDetails)
+            .then(res => res.data)
+            .catch(error => {
+                toast.error("Checkout Failed!!");
+                console.log("Error during Checkout: ", error);
+            });
+    }
+}
