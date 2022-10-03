8fed1f148f38d7637ffbb3bbd880d81c0337a212
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-03 13:41:47 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const products = require("./products.json");
-
+const { validateCartItems } = require("use-shopping-cart/src/serverUtil");
 
 module.exports = function getRoutes() {
   const router = express.Router();
@@ -9,12 +9,12 @@ module.exports = function getRoutes() {
   router.get("/products", getProducts);
   router.get("/products/:productId", getSingleProduct);
 
-  router.post('/checkout-sessions', createCheckoutSession);
+  router.post("/checkout-sessions", createCheckoutSession);
 
   return router;
 };
 
-// controlers 
+// controlers
 function getProducts(req, res) {
   res.status(200).json({ products });
 }
@@ -29,10 +29,30 @@ function getSingleProduct(req, res) {
     }
     res.status(200).json({ product });
   } catch (err) {
-    return res.status(404).json({ statusCode: 404,  message: error.message });
+    return res.status(404).json({ statusCode: 404, message: error.message });
   }
 }
 
-function createCheckoutSession() {
-  
+function createCheckoutSession(req, res) {
+  try {
+    const cartItems = req.body;
+    const line_items = validateCartItems(products, cartItems);
+
+    const origin =
+      process.env.NODE_ENV === "production"
+        ? req.headers.origin
+        : "http://localhost:3000";
+
+    const params = {
+      submit_type: "pay",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      // shipping_address_collection: {
+      //   allowed_countries: ["US", "IN", ]
+      // },
+      line_items,
+      success_url: `${origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: origin,
+    };
+  } catch (error) {}
 }
