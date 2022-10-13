e8f150bfd62266e675dc0db16aff96475b29ecf8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-13 13:49:14 +0530

    Update AddToCart.js

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -23,13 +23,13 @@ export default function AddToCart({ product, currentProduct }) {
         </button>
       ) : (
         <div className="flex border-white ml-auto pr-0">
-          <button className="flex text-white bg-blue-900 border-gray-50 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-l font-normal">
+          <button className="flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-l font-normal">
             -
           </button>
           <span className="flex justify-center items-center text-xs w-10 h-100 font-semibold text-blue-800 bg-white ">
             {currentProduct[0].quantity}
           </span>
-          <button className="flex text-white bg-blue-900 border-gray-50 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-r font-normal">
+          <button className="flex text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-r font-normal">
             +
           </button>
         </div>
