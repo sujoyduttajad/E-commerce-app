fc2728880b3dc05c20b393899a6a7899b7094d47
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-13 12:20:29 +0530

    Update AddToCart.js

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -13,7 +13,7 @@ export default function AddToCart({ product }) {
   return (
     <button
       className="flex ml-auto text-white bg-blue-900 border-0 py-2 px-6 
-      focus:outline-none hover:bg-blue-800 rounded"
+      focus:outline-none hover:bg-blue-800 rounded font-medium"
       onClick={handleAddItem}
     >
       Add To Cart
