a4aff933dc2a8765d9caa40eccad9a6d4cdfc3ef
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-13 13:20:39 +0530

    Update AddToCart.js

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -10,6 +10,7 @@ export default function AddToCart({ product, currentProduct }) {
     toast.success(`${product.name} is added to your cart!`);
   };
 
+
   return (
     <>
       {currentProduct.length === 0 ? (
@@ -21,11 +22,14 @@ export default function AddToCart({ product, currentProduct }) {
           Add To Cart
         </button>
       ) : (
-        <div className="flex border-white ml-auto px-6">
-          <button className="flex  text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-l font-normal">
+        <div className="flex border-white ml-auto px-6 pr-0">
+          <button className="flex text-white bg-blue-900 border-gray-50 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-l font-normal">
             -
           </button>
-          <button className="flex ml-auto text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-r font-normal">
+          <span className="flex justify-center items-center text-xs w-10 h-100 font-semibold text-blue-800 bg-white ">
+            {currentProduct[0].quantity}
+          </span>
+          <button className="flex text-white bg-blue-900 border-gray-50 py-2 px-6 focus:outline-none hover:bg-blue-800 rounded-r font-normal">
             +
           </button>
         </div>
