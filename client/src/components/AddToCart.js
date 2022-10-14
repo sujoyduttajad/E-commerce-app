7cf2dc571eacf5641c1c9f69769b8b399f42a519
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-14 16:35:44 +0530

    Implemented Add and Minus products functionality

--- client/src/components/AddToCart.js
+++ client/src/components/AddToCart.js
@@ -3,12 +3,20 @@ import toast from "react-hot-toast";
 import { useShoppingCart } from "use-shopping-cart";
 
 export default function AddToCart({ product, currentProduct }) {
-  const { addItem } = useShoppingCart();
+  const { addItem, incrementItem, decrementItem } = useShoppingCart();
 
   const handleAddItem = () => {
     addItem(product);
     toast.success(`${product.name} is added to your cart!`);
   };
+  
+  const handleIncrement = (id) => {
+    incrementItem(id)
+  }
+
+  const handleDecremnet = (id) => {
+    decrementItem(id)
+  }
 
   // We need to include functionality to increment and decrement quantity
 
@@ -24,16 +32,22 @@ export default function AddToCart({ product, currentProduct }) {
         </button>
       ) : (
         <div className="flex border-white ml-auto pr-0">
-          <button className="flex text-lg text-white bg-blue-900 border-0 py-1 px-6 
-          focus:outline-none hover:bg-blue-800 rounded-l font-normal">
+          <button 
+            className="flex text-lg text-white bg-blue-900 border-0 py-1 px-6 
+          focus:outline-none hover:bg-blue-800 rounded-l font-normal"
+          onClick={() => handleDecremnet(product.id)}
+          >
             -
           </button>
           <span className="flex justify-center items-center text-xs w-10 h-100 
           font-semibold text-blue-800 bg-white ">
             {currentProduct[0].quantity}
           </span>
-          <button className="flex text-lg text-white bg-blue-900 border-0 py-1 px-6 
-          focus:outline-none hover:bg-blue-800 rounded-r font-normal">
+          <button 
+            className="flex text-lg text-white bg-blue-900 border-0 py-1 px-6 
+          focus:outline-none hover:bg-blue-800 rounded-r font-normal"      
+            onClick={() => handleIncrement(product.id)}
+          >
             +
           </button>
         </div>
