9aa3532fa3b086cc2d16515d7b3f8eb2b3b7846d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-15 17:49:53 +0530

    Update CartModal.js

--- client/src/components/CartModal.js
+++ client/src/components/CartModal.js
@@ -23,6 +23,7 @@ export default function CartModal({ isModalOpen, toggleModal }) {
       closeTimeoutMS={500}
       shouldCloseOnOverlayClick={true}
       shouldCloseOnEsc={true}
+      preventScroll={false}
     >
       <div
         className="flex items-center justify-center fixed left-0 bottom-0 w-full 
