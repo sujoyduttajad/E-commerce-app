05717e2ca06c35cbb6156b5fefb9c0444dfb1294
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-15 17:49:49 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,15 +7,18 @@ import { LogoIcon } from "./Icons";
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-30 w-full text-gray-400 bg-gray-900 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link to="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+      <div className="container mx-auto flex flex-wrap justify-between p-5 flex-col md:flex-row items-center">
+        <Link
+          to="/"
+          className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+        >
           <span className="flex items-center justify-center ml-3 text-xl">
             <LogoIcon paramWidth="" paramHeight="" />
           </span>
         </Link>
         <div className="flex flex-nowrap">
-        <CartSummary />
-        <CheckoutCart />
+          <CartSummary />
+          <CheckoutCart />
         </div>
       </div>
     </header>
