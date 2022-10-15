45488230c0e31e61396c7bfc323999bf185b59f2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-15 17:37:30 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,12 +7,23 @@ module.exports = {
         white: "#ffffff",
         purple: "#3f3cbb",
         midnight: "#121063",
-        // 'metal': '#565584',
-        // 'tahiti': '#3ab7bf',
-        // 'silver': '#ecebff',
-        // 'bubble-gum': '#ff77e9',
-        // 'bermuda': '#78dcca',
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
 };
