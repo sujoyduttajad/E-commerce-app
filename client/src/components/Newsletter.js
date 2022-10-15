7bd77919bb19d44acaff4085bdd97aa3fbb37387
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-15 18:22:59 +0530

    Update Newsletter.js

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Newsletter = () => {
   return (
-    <section className='flex bg-blue-700 h-auto'>
-        <div>
-            <div className='flex flex-col'>
-                <h2>Newsletter</h2>
-                <p>Subscribe here to get every single updates</p>
-            </div>
+    <section className="w-full flex items-center justify-center bg-gray-800 h-auto">
+      <div>
+        <div className="flex flex-col">
+          <h2 className="tracking-wider text-xs title-font font-medium text-gray-500 mb-1 mt-2 text-lg">
+            Newsletter
+          </h2>
+          <p>Subscribe here to get every single updates</p>
         </div>
-        <form>
-            <input />
-            <button>Subscribe Now</button>
-        </form>
+      </div>
+      <form>
+        <input />
+        <button>Subscribe Now</button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
