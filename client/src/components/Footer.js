f94f0458a28e247181b3a3576c6ff336b2425ae5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-15 12:26:54 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { LogoIcon } from "./Icons";
 
 const Footer = () => {
   return (
-    <footer>
-        <div>
-            <h1>Hello Footer</h1>
-        </div>
+    <footer className="flex h-48 p-6">
+      <div className="w-auto">
+        <Link
+          to="/"
+          className="flex flex-col title-font items-start text-white m-12 md:mb-0"
+        >
+          <span className="flex items-center justify-center ml-3">
+            <LogoIcon />
+          </span>
+          <h2 className="mt-6 pl-5 text-lg">Primart Inc. Pvt. limited</h2>
+        </Link>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
