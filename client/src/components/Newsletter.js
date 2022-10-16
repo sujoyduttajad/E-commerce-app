2b63349d19f4830ff6f67b6b188919a73ced08fe
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-16 12:21:49 +0530

    Update Newsletter.js

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -5,7 +5,7 @@ const Newsletter = () => {
   return (
     <section className="w-full flex flex-wrap items-center justify-center bg-gray-800 h-48">
       <div className="flex p-3 items-center ">
-      <MailIcon paramWidth="" paramHeight="" />
+        <MailIcon paramWidth="" paramHeight="" />
         <div className="flex flex-col p-4 mr-5">
           <h2 className="tracking-wider text-xs title-font font-medium text-white text-4xl">
             Newsletter
@@ -35,29 +35,6 @@ const Newsletter = () => {
             Subscribe Now
           </button>
         </form>
-        {/* 
-        <form>
-          <div className="relative">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <Envelope />
-            </div>
-            <input
-              type="search"
-              id="search"
-              placeholder="johndoe@gmail.com"
-              required
-              className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg 
-              border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-            />
-            <button
-              type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 
-              focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
-            >
-              Subscribe Now
-            </button>
-          </div>
-        </form> */}
       </div>
     </section>
   );
