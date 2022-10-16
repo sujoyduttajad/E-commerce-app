2a77467e4a7f4060007a644bf787c1cd323e141f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-16 16:14:41 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,6 +16,28 @@ export default function Navbar() {
             <LogoIcon paramWidth="" paramHeight="" />
           </span>
         </Link>
+        {/* Search Bar */}
+        <div className="relative">
+          <form className="w-96">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3">
+              
+            </div>
+            <input
+              type="text"
+              id="input-group-1"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm leading-loose 
+            rounded focus:ring-blue-500 focus:outline-blue-500 min-w-96 w-full pl-10 p-2.5 pr-"
+              placeholder="Search here"
+            />
+            <button
+              className="flex absolute right-1.5 bottom-1.5 items-center bg-blue-900 border-0 px-3 
+                focus:outline-none hover:bg-blue-800 rounded text-white md:mt-0 
+                focus:ring-4 focus:ring-blue-300 font-medium text-md px-4 py-2"
+            >
+              Search
+            </button>
+          </form>
+        </div>
         <div className="flex flex-nowrap">
           <CartSummary />
           <CheckoutCart />
