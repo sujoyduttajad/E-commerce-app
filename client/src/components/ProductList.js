c5db9c71d70508259a181ffd48dc396f869f8706
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-17 20:41:12 +0530

    Update ProductList.js

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -25,8 +25,8 @@ function ProductItem({ product }) {
   const price = formatProductPrice(product);
 
   return (
-    <div className="p-4 md:w-1/3">
-      <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
+    <div className=" p-4 md:w-1/3">
+      <div className="product-card h-full border-2 border-gray-800 rounded-lg overflow-hidden">
         <Link to={`/${product.id}`}>
           <img
             className="lg:h-96 md:h-36 w-full object-cover object-center"
