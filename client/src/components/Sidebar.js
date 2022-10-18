63857fdc71564a77773829cbea8ce4579578fc22
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-19 02:34:55 +0530

    Update Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { UserIcon } from "./Icons";
+import { ShoppingBag, UpgradePro, UserIcon } from "./Icons";
 
 const Sidebar = () => {
   return (
@@ -48,7 +48,7 @@ const Sidebar = () => {
               href="#"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
             >
-
+                <ShoppingBag />
               <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
             </a>
           </li>
@@ -59,21 +59,7 @@ const Sidebar = () => {
               href="#"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
             >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white dark:text-gray-400"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="gem"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z"
-                ></path>
-              </svg>
+                <UpgradePro />
               <span className="ml-4">Upgrade to Pro</span>
             </a>
           </li>
