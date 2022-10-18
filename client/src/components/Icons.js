50173cefba955f2cdedad0cb71ec77c6ace400c9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-19 02:48:00 +0530

    Update Icons.js

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -157,16 +157,17 @@ export function SearchGrayIcon({ paramWidth, paramHeight }) {
 export function UserIcon() {
   return (
     <svg
-      aria-hidden="true"
-      className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-      fill="currentColor"
-      viewBox="0 0 20 20"
+      className="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-        clip-rule="evenodd"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
       ></path>
     </svg>
   );
