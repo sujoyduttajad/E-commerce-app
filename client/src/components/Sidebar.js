0eb7c475d15cf1fc5c868f8708bf6a2954f52c25
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-19 04:08:34 +0530

    Update Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -23,7 +23,7 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
       preventScroll={false}
     >
       <aside className="w-64 h-full mt-8" aria-label="Sidebar">
-        <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+        <div className="h-full overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
           <ul className="space-y-2">
             <li>
               <a
