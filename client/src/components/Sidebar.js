8cdde2d236e0b3608ee82ed8695d5f54035df7b1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-19 12:32:03 +0530

    Update Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -11,6 +11,7 @@ import {
   UserIcon,
 } from "./Icons";
 import Modal from "react-modal";
+import { Link } from "react-router-dom";
 
 const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
   return (
@@ -24,95 +25,95 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
       preventScroll={true}
       portalClassName="ReactModalPortal"
     >
-      <aside className="sidebar w-64 h-full mt-8" aria-label="Sidebar">
+      <aside className="sidebar w-64 h-5/6 mt-8 rounded" aria-label="Sidebar">
         <div className="h-full overflow-y-auto py-4 px-3 rounded">
           <ul className="space-y-2">
             <li>
-              <a
-                href="#"
+            <Link
+                to="/OrderTracking"
                 className="flex items-center p-2 text-base font-normal rounded-lg   hover:bg-gray-800"
               >
                 <OrdersIcon />
                 <span className="ml-3">Your Orders</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="/Account"
                 className="flex items-center p-2 text-base font-normal rounded-lg   hover:bg-gray-800"
               >
                 <UserIcon />
                 <span className="flex-1 ml-3 whitespace-nowrap">
                   Your Profile
                 </span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="#"
                 className="flex items-center p-2 text-base font-normal rounded-lg   hover:bg-gray-800"
               >
                 <Notifications />
                 <span className="flex-1 ml-3 whitespace-nowrap">
                   Notifications
                 </span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="#"
                 className="flex items-center p-2 text-base font-normal rounded-lg   hover:bg-gray-800"
               >
                 <ShoppingBag />
                 <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-              </a>
+              </Link>
             </li>
           </ul>
           <ul className="pt-4 mt-4 space-y-2 border-t border-gray-600 dark:border-gray-700">
             <li>
-              <a
-                href="#"
+              <Link
+                 to="/premium"
                 className="flex items-center p-2 text-base font-normal rounded-lg transition duration-75   hover:bg-gray-800 group"
               >
                 <UpgradePro />
                 <span className="ml-4">Upgrade to Pro</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="/AboutUs"
                 className="flex items-center p-2 text-base font-normal rounded-lg transition duration-75   hover:bg-gray-800 group"
               >
                 <InfoIcon />
                 <span className="ml-3">About Us</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="/support/help"
                 className="flex items-center p-2 text-base font-normal rounded-lg transition duration-75   hover:bg-gray-800 group"
               >
                 <HelpIcon />
                 <span className="ml-3">Help</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="/login"
                 className="flex items-center p-2 text-base font-normal rounded-lg   hover:bg-gray-800"
               >
                 <LogInIcon />
                 <span className="flex-1 ml-3 whitespace-nowrap">Log In</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                 to="/signup"
                 className="flex items-center p-2 text-base font-normal rounded-lg   hover:bg-gray-800"
               >
                 <SignUpIcon />
                 <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
