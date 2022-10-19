52d2928e61bc7daccc4cf002aecfbd55c4063164
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-19 12:04:01 +0530

    Update Icons.js

--- client/src/components/Icons.js
+++ client/src/components/Icons.js
@@ -158,7 +158,7 @@ export function UserIcon() {
   return (
     <svg
       className="w-6 h-6"
-      fill="none"
+      fill="#ff9a62"
       stroke="currentColor"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
@@ -178,7 +178,7 @@ export function ShoppingBag() {
     <svg
       aria-hidden="true"
       className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -202,9 +202,9 @@ export function UpgradePro() {
       role="img"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
+      fill="#ff9a62"
     >
       <path
-        fill="currentColor"
         d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z"
       ></path>
     </svg>
@@ -215,7 +215,7 @@ export function InfoIcon() {
   return (
     <svg
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -232,7 +232,7 @@ export function MenuIcon() {
   return (
     <svg
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -250,7 +250,7 @@ export function SignUpIcon() {
     <svg
       aria-hidden="true"
       className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -268,7 +268,7 @@ export function LogInIcon() {
     <svg
       aria-hidden="true"
       className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -285,7 +285,7 @@ export function Notifications() {
   return (
     <svg
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -298,7 +298,7 @@ export function HelpIcon() {
   return (
     <svg
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#ff9a62"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -307,6 +307,20 @@ export function HelpIcon() {
         d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
         clipRule="evenodd"
       />
+    </svg>
+  );
+}
+
+export function OrdersIcon() {
+  return (
+    <svg
+      className="w-6 h-6"
+      fill="#ff9a62"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path 
+      d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
     </svg>
   );
 }
