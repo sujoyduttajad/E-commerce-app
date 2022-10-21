21bee3b7f2d659d1b3b8a10e5e155f8ac19b5e4d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-10-21 18:23:17 +0530

    Update RemoveFromCart.js

--- client/src/components/RemoveFromCart.js
+++ client/src/components/RemoveFromCart.js
@@ -6,8 +6,10 @@ export default function RemoveFromCart({ product, currentProduct }) {
   const { removeItem, cartCount } = useShoppingCart();
 
   const handleRemoveItem = () => {
-    removeItem(product.id);
-    toast.success(`${product.name} is removed from your cart!`);
+    if(currentProduct.length !== 0) {
+      removeItem(product.id);
+      toast.success(`${product.name} is removed from your cart!`);
+    }
   };
 
   return (
@@ -15,7 +17,7 @@ export default function RemoveFromCart({ product, currentProduct }) {
       className="flex ml-2 text-indigo-900 bg-yellow-100 border-0 py-2 px-6 
       focus:outline-none hover:bg-yellow-200 rounded font-medium"
       onClick={handleRemoveItem}
-      disabled={currentProduct.length !== 0}
+      disabled={currentProduct.length === 0}
       style={{
         cursor: `${currentProduct.length !== 0 ? "pointer" : "not-allowed"}`,
         opacity: `${currentProduct.length !== 0 ? 1 : 0.5}`,
