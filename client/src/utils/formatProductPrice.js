742907e41c257bbe73a9d3a001c6ef12ad249ecc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-11-21 15:18:32 +0530

    Update formatProductPrice.js

--- client/src/utils/formatProductPrice.js
+++ client/src/utils/formatProductPrice.js
@@ -3,7 +3,7 @@ import { formatCurrencyString } from "use-shopping-cart";
 export default function formatProductPrice(product) {
     return formatCurrencyString({
         value: product.price,
-        currency: product.currency,
+        currency: "USD",
         language: navigator.language,
     });
 
