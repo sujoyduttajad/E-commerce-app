1a86a15dc55def23c0488eb983a227a80508c4c2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-05-13 18:43:47 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { CartProvider } from "use-shopping-cart";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <App />, 
+  <BrowserRouter>
+    <CartProvider
+      mode="payment"
+      cartMode="checkout-session"
+      stripe={process.env.REACT_APP_STRIPE_KEY}
+      currency="USD"
+      allowedCountries={["US", "GB", "CA"]}
+      billingAddressCollection={true}
+      // successUrl="stripe.com"
+      // cancelUrl="twitter.com/dayhaysoos"
+    >
+      <App />
+    </CartProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
