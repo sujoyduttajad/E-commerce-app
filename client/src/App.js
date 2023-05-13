7b25cd60e65cad274827d3eb5e16861717fcc10f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-05-13 16:41:50 +0530

    Moved CartProvider and Router to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Result from "./pages/Result";
 import Product from "./pages/Product";
 import { QueryClientProvider, QueryClient } from "react-query";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "use-shopping-cart";
 import { Toaster } from "react-hot-toast";
 import Navbar from "components/Navbar";
@@ -13,24 +13,18 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <CartProvider
-        cartMode="checkout-session"
-        stripe={process.env.REACT_APP_STRIPE_KEY}
-        currency="USD"
-      >
-        <Navbar />
-        <QueryClientProvider client={queryClient}>
-          <Toaster position="bottom-center" />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/result" component={Result} />
-            <Route path="/:productId" component={Product} />
-          </Switch>
-        </QueryClientProvider>
-        <Footer />
-      </CartProvider>
-    </BrowserRouter>
+    <React.Fragment>
+      <Navbar />
+      <QueryClientProvider client={queryClient}>
+        <Toaster position="bottom-center" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/:productId" element={<Product />} />
+        </Routes>
+      </QueryClientProvider>
+      <Footer />
+    </React.Fragment>
   );
 }
 
